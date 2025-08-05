b8144b396afec61c9273afc4da97073f0f247160
quuavke <xxx@gmail.com>
2025-08-06 02:03:24 +0700

    Add GetByID test for gorm book repo

--- pkg/repositories/book_repository_test.go
+++ pkg/repositories/book_repository_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func TestBookRepo_GetAll(t *testing.T) {
@@ -20,7 +21,9 @@ func TestBookRepo_GetAll(t *testing.T) {
 
     gormDB, err := gorm.Open(postgres.New(postgres.Config{
         Conn: db,
-    }), &gorm.Config{})
+    }), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
     assert.NoError(t, err)
 
     repo := repositories.NewGormBookRepo(gormDB)
@@ -100,6 +103,75 @@ func TestBookRepo_GetAll(t *testing.T) {
     assert.Equal(t, "Clean Code", books[1].Title)
     assert.Equal(t, "Robert", books[1].Author.Firstname)
     assert.Equal(t, "Martin", books[1].Author.Lastname)
+
+    assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestBookRepo_GetByID(t *testing.T) {
+    db, mock, err := sqlmock.New()
+    assert.NoError(t, err)
+    defer db.Close()
+
+    gormDB, err := gorm.Open(postgres.New(postgres.Config{
+        Conn: db,
+    }), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+    assert.NoError(t, err)
+
+    repo := repositories.NewGormBookRepo(gormDB)
+
+    now := time.Now().UTC()
+    testBook := models.Book{
+        ID:        1,
+        Title:     "Book get by id 1",
+				Content:   "Test id 1",
+        CreatedAt: now,
+        UpdatedAt: now,
+        Author: models.Author{
+            Firstname: "Alan",
+            Lastname:  "Doe",
+            Birthday:  now,
+						CreatedAt: now,
+						UpdatedAt: now,
+          },
+        }
+    
+
+
+    query := regexp.QuoteMeta(`SELECT * FROM "books"`)
+    
+    row := sqlmock.NewRows([]string{
+        "id", "title", "content", "created_at", "updated_at",
+        "author_firstname", "author_lastname", "author_birthday", "author_created_at", "author_updated_at",
+    })
+    
+    row.AddRow(
+        testBook.ID,
+        testBook.Title,
+				testBook.Content,
+        testBook.CreatedAt,
+        testBook.UpdatedAt,
+        testBook.Author.Firstname,
+        testBook.Author.Lastname,
+        testBook.Author.Birthday,
+				testBook.Author.CreatedAt,
+				testBook.Author.UpdatedAt,
+    )
+    
+
+    mock.ExpectQuery(query).WillReturnRows(row)
+
+    book, err := repo.GetByID(1)
+
+    assert.NoError(t, err)
+    
+    // Проверяем первую книгу
+    assert.Equal(t, 1, book.ID)
+    assert.Equal(t, "Book get by id 1", book.Title)
+    assert.Equal(t, "Alan", book.Author.Firstname)
+    assert.Equal(t, "Doe", book.Author.Lastname)
+  
 
     assert.NoError(t, mock.ExpectationsWereMet())
 }
